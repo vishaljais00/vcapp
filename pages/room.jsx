46237bbf2dfcb14d6fc05bec59834503f72164ff
vishal jais <vishal.jais00@gmail.com>
2024-01-08 11:35:49 +0530

    layout

--- pages/room.jsx
+++ pages/room.jsx
@@ -1,53 +1,60 @@
 // pages/Room.js
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 // const { ZegoUIKitPrebuilt } = await import("@zegocloud/zego-uikit-prebuilt");
 
 const Room = () => {
-  useEffect(async() => {
-    // import("@zegocloud/zego-uikit-prebuilt").then(({ ZegoUIKitPrebuilt }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const asyncFn = async () =>{
       const { ZegoUIKitPrebuilt } = await import("@zegocloud/zego-uikit-prebuilt");
-        function getUrlParams(url) {
-          let urlStr = url.split('?')[1];
-          const urlSearchParams = new URLSearchParams(urlStr);
-          const result = Object.fromEntries(urlSearchParams.entries());
-          return result;
-        }
+      function getUrlParams(url) {
+        let urlStr = url.split('?')[1];
+        const urlSearchParams = new URLSearchParams(urlStr);
+        const result = Object.fromEntries(urlSearchParams.entries());
+        return result;
+      }
 
-    const roomID = getUrlParams(window.location.href)['roomID'] || (Math.floor(Math.random() * 10000) + "");
-    const userID = Math.floor(Math.random() * 10000) + "";
-    const userName = "userName" + userID;
-    // const appID = 1484647939;
-    const appID = 1051992223;
-    const serverSecret = "97fd45081621ff415c72452e3dd5adbd";
-    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, userID, userName);
+      const roomID = getUrlParams(window.location.href)['roomID'] || (Math.floor(Math.random() * 10000) + "");
+      const userID = Math.floor(Math.random() * 10000) + "";
+      const userName = "userName" + userID;
+      // const appID = 1484647939;
+      const appID = 1051992223;
+      const serverSecret = "97fd45081621ff415c72452e3dd5adbd";
+      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, userID, userName);
 
-    const zp = ZegoUIKitPrebuilt.create(kitToken);
-    zp.joinRoom({
-      container: document.querySelector("#rootRoom"),
-      sharedLinks: [{
-        name: 'Personal link',
-        url: window.location.protocol + '//' + window.location.host + window.location.pathname + '?roomID=' + roomID,
-      }],
-      scenario: {
-        mode: ZegoUIKitPrebuilt.VideoConference,
-      },
-      turnOnMicrophoneWhenJoining: true,
-      turnOnCameraWhenJoining: true,
-      showMyCameraToggleButton: true,
-      showMyMicrophoneToggleButton: true,
-      showAudioVideoSettingsButton: true,
-      showScreenSharingButton: true,
-      showTextChat: true,
-      showUserList: true,
-      maxUsers: 2,
-      layout: "Auto",
-      showLayoutButton: false,
-    });
-  // })
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
+      zp.joinRoom({
+        container: document.querySelector("#rootRoom"),
+        sharedLinks: [{
+          name: 'Personal link',
+          url: window.location.protocol + '//' + window.location.host + window.location.pathname + '?roomID=' + roomID,
+        }],
+        scenario: {
+          mode: ZegoUIKitPrebuilt.OneONoneCall,
+        },
+        onLeaveRoom: () => {
+          router.push('/')
+        },
+        turnOnMicrophoneWhenJoining: true,
+        turnOnCameraWhenJoining: true,
+        showMyCameraToggleButton: true,
+        showMyMicrophoneToggleButton: true,
+        showAudioVideoSettingsButton: true,
+        showScreenSharingButton: false,
+        showTextChat: true,
+        showUserList: false,
+        maxUsers: 2,
+        layout: "Grid",
+        showLayoutButton: false,
+        showPreJoinView: false,
+      });
+    }
+    asyncFn();      
   }, []);
 
   return (
-    <div id="rootRoom" className="myCallContainer"></div>
+    <div id="rootRoom" className="myCallContainer h-[100vh]"></div>
   );
 };
 
